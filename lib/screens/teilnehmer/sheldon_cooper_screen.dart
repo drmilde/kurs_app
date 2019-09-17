b9a86eaa23f9c835e2eb40c3f72a87bef733cde1
milde <milde@hs-fulda.de>
2019-09-17 17:55:15 +0200

    ein paar kommas

--- lib/screens/teilnehmer/sheldon_cooper_screen.dart
+++ lib/screens/teilnehmer/sheldon_cooper_screen.dart
@@ -4,20 +4,22 @@ class SheldonCooperScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Dr. Dr. Sheldon Cooper")),
-        body: Container(
-            color: Colors.white,
-            child: GridView.count(
-              // Create a grid with 2 columns. If you change the scrollDirection to
-              // horizontal, this produces 2 rows.
-              crossAxisCount: 2,
-              // Generate 100 widgets that display their index in the List.
-              children: List.generate(100, (index) {
-                return Center(
-                  child: Image.network(
-                      "https://i0.web.de/image/568/32000568,pd=2/sheldon-cooper-jim-parsons-the-big-bang-theory.jpg"),
-                );
-              }),
-            )));
+      appBar: AppBar(title: Text("Dr. Dr. Sheldon Cooper")),
+      body: Container(
+        color: Colors.white,
+        child: GridView.count(
+          // Create a grid with 2 columns. If you change the scrollDirection to
+          // horizontal, this produces 2 rows.
+          crossAxisCount: 2,
+          // Generate 100 widgets that display their index in the List.
+          children: List.generate(100, (index) {
+            return Center(
+              child: Image.network(
+                  "https://i0.web.de/image/568/32000568,pd=2/sheldon-cooper-jim-parsons-the-big-bang-theory.jpg"),
+            );
+          }),
+        ),
+      ),
+    );
   }
 }
